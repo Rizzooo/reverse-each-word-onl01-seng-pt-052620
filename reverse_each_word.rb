71f7499a8ddc6055ef4e130096e2c033d5b02b0d
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-04 18:22:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   array1 = sentence1.split(" ")
   
-  reversed_sentence = array1.map(&:reverse!)
+  reversed_sentence = array1.collect(&:reverse!)
   
   reversed_sentence.join(" ")
 end
